9e663a3797e622612111dc75a12b3787deb75cea
ArgyleMonster <krou7172@vandals.uidaho.edu>
2018-05-09 17:04:02 -0700

    Modified: board.js 	created cellList{} in Board class 	created fillBox() to fill a box 	moved topLeftCell to Board class

--- board.js
+++ board.js
@@ -1,24 +1,40 @@
 class Board {
   constructor(){
+    this.cellList = {};
   }
 
   generateCellClasses(){
     for (var x = 0; x <= 8; x++) {
       for (var y = 0; y <= 8; y++) {
-        var className = "cell" + x + y;
-        this[className] = new Cell(x,y);
+        var cellName = "cell" + x + y;
+        this.cellList.cellName = new Cell(x,y);
+        console.log(this.cellList.cellName);
       }
     }
   }
 
   fillFirstThreeBoxes() { //Fill three diagonal boxes
-    var boxesToFill = [['cell',0,0],['cell',3,3],['cell',6,6]]
+    var boxesToFill = [['cell',0,0],['cell',3,3],['cell',6,6]];
     var numOfBoxes = boxesToFill.length;
-    for (var i = 0; i <= numOfBoxes; i++;){
-      for (var y = 0; y < 3; y++){
-        for (var x = 0; x < 3; x++){
-          
+    for (var i = 0; i <= numOfBoxes; i++){
+      var yOriginal = boxesToFill[0][2];
+      for (var y = boxesToFill[0][2]; y < yOriginal + 3; y++){
+        for (var x = boxesToFill[0][1]; x < 3; x++){
+          var currentCell = this.cell(x,y);
+          console.log(currentCell);
         }
+      }
+    }
+  }
+
+  fillBox(x,y){
+    var topLeftCell = this.topLeftCell(x,y);
+    var topLeftCellX = topLeftCell.charAt(0);
+    var topLeftCellY = topLeftCell.charAt(1);
+    for (this.y = topLeftCellY; y < 3; this.y++){
+      for (this.x = topLeftCellX; x < 3; this.x++){
+          var currentCell = this.cell(this.x,this.y);
+          console.log(currentCell);
       }
     }
   }
@@ -40,46 +56,9 @@ class Board {
     }
   }
 
-  cell(x,y) {
-    return this["cell" + x + y];
-  }
-}
-
-class Cell extends Board {
-  constructor(x,y){
-    var cellNum; //final generated number for the cell
-    super(cellNum);
-    this.availableNumList = [1,2,3,4,5,6,7,8,9];
-    this.cellID = "" + x + y;
-  }
-
-  removeNumber(numToRemove) {
-    var loc = this.availableNumList.indexOf(numToRemove);
-    if (loc > -1) {
-      this.availableNumList.splice(loc, 1);
-    }
-  }
-
-  reAddNumber(numToAdd) {
-    if (numToAdd > 0 && numToAdd < 10) {
-      console.log(this.availableNumList.indexOf(numToAdd));
-      this.availableNumList.push(numToAdd);
-    }
-  }
-
-  populate() { //push final number to the cell
-    document.getElementById(this.cellID).innerHTML = this.cellNum;
-  }
-
-  generateNum() {
-    var randLoc = Math.floor(Math.random() * this.availableNumList.length);
-    this.cellNum = this.availableNumList[randLoc];
-    this.removeNumber(this.cellNum);
-  }
-
-  get topLeftCell(){ // returns two digit number (ie. 36)
-    this.x = this.cellName.charAt(0);
-    this.y = this.cellName.charAt(1);
+  topLeftCell(x,y){ // returns two digit number (ie. 66)
+    this.x = x;
+    this.y = y;
     var foundX = false;
     var foundY = false;
     var test = 0;
@@ -101,6 +80,42 @@ class Cell extends Board {
     }
     return "" + this.x + this.y;
   }
+
+}
+
+class Cell extends Board {
+  constructor(x,y){
+    var cellNum; //final generated number for the cell
+    super(cellNum);
+    this.availableNumList = [1,2,3,4,5,6,7,8,9];
+    this.cellID = "" + x + y;
+    this.htmlTD = document.getElementById(this.cellID);
+  }
+
+  removeNumber(numToRemove) {
+    var loc = this.availableNumList.indexOf(numToRemove);
+    if (loc > -1) {
+      this.availableNumList.splice(loc, 1);
+    }
+  }
+
+  reAddNumber(numToAdd) {
+    if (numToAdd > 0 && numToAdd < 10) {
+      console.log(this.availableNumList.indexOf(numToAdd));
+      this.availableNumList.push(numToAdd);
+    }
+  }
+
+  populate() { //push final number to the cell
+     this.htmlTD.innerHTML = this.cellNum;
+  }
+
+  generateNum() {
+    var randLoc = Math.floor(Math.random() * this.availableNumList.length);
+    this.cellNum = this.availableNumList[randLoc];
+    this.removeNumber(this.cellNum);
+  }
+
 
   get availableNum(){
     return this.availableNumList;
